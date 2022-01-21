9ee2da8dceed292d4a7ae7bbec6519ff13552611
renguangli <renguangli@bonc.com.cn>
2022-01-21 17:16:31 +0800

    spring-cloud-gateway

---
+++ spring-cloud-gateway/src/main/java/com/renguangli/gateway/config/DiscoveryConfiguration.java
@@ -0,0 +1,15 @@
+package com.renguangli.gateway.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * spring-cloud-alibaba-samples
+ * Created by renguangli at 2022/1/21 4:44 下午
+ *
+ * @since JDK1.8
+ */
+@Configuration
+@EnableDiscoveryClient
+public class DiscoveryConfiguration {
+}
